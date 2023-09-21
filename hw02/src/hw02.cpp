0d4966c210c538d26fdcafd607416c8d82fea36b
Niyaz Imamov <niyaz120904@gmail.com>
2023-09-22 00:12:09 +0300

    Add hw02

--- hw02/src/hw02.cpp
+++ hw02/src/hw02.cpp
@@ -1,7 +1,26 @@
 #include <hw02.h>
 
-std::string hw02(std::string input_str)
-{
-    // Write code here..
-    return input_str;
+bool contains(std::string, char);
+
+std::string hw02(std::string input_str){
+    std::string output_str{};
+
+    for(int i = 0; i < input_str.length(); i++)
+    {
+        if(!contains(output_str, input_str[i])){
+            output_str.push_back(input_str[i]);
+        }
+    }
+    return output_str;
 }
+
+bool contains(std::string str, char ch){
+    bool flag = false;
+    for(int i = 0; i < str.length(); i++){
+        if(str[i] == ch){
+            flag = true;
+            break;
+        }
+    }
+    return flag;
+ }
