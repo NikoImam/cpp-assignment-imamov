be422bb9cc2cc9d077dd942acd8414728a2be561
Niyaz Imamov <niyaz120904@gmail.com>
2023-10-04 04:45:40 +0300

    Add hw04 without memory optimisation

--- hw04/src/hw04.cpp
+++ hw04/src/hw04.cpp
@@ -1,7 +1,28 @@
 #include <hw04.h>
+#include <new>
+#include <iostream>
 
 std::size_t hw04(const int* input_array, std::size_t elem_count)
 {
+    int *out_data = new int[1];
     std::size_t sum = 0;
+    std::size_t c = 0;
+    int *temp = new int[1];
+    for(std::size_t i = 0; i < elem_count; i++)
+    {
+        auto elem = *(input_array + i);
+        if(elem < 0) break;
+        out_data = (int*)realloc(out_data, i + 1);
+        *(out_data + i) = elem;
+        c++;
+    }
+    
+    for(std::size_t i = 0; i < c; i++)
+    {
+        sum += *(out_data + i);
+    }
+    delete[] out_data;
+    //delete[] temp;
+    out_data = nullptr;
     return sum;
 }
