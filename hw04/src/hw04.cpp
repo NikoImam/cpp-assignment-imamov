4f7ffe8647e77b531b0d71add3c61b9a837e67dc
Niyaz Imamov <niyaz120904@gmail.com>
2023-10-04 13:25:40 +0300

    Add ready hw04 with memory optimisation

--- hw04/src/hw04.cpp
+++ hw04/src/hw04.cpp
@@ -1,28 +1,49 @@
 #include <hw04.h>
-#include <new>
-#include <iostream>
 
 std::size_t hw04(const int* input_array, std::size_t elem_count)
 {
     int *out_data = new int[1];
+
     std::size_t sum = 0;
-    std::size_t c = 0;
+    std::size_t count = 0;
+
     int *temp = new int[1];
+
     for(std::size_t i = 0; i < elem_count; i++)
     {
         auto elem = *(input_array + i);
         if(elem < 0) break;
-        out_data = (int*)realloc(out_data, i + 1);
+
+        delete[] out_data;
+        out_data = new int[i + 1];
+
+        for(std::size_t j = 0; j < i; j++)
+        {
+            *(out_data + j) = *(temp + j);
+        }
+
         *(out_data + i) = elem;
-        c++;
+
+        delete[] temp;
+        temp = new int[i + 1];
+
+        for(std::size_t j = 0; j < i + 1; j++)
+        {
+            *(temp + j) = *(out_data + j);
+        }
+
+        count++;
     }
     
-    for(std::size_t i = 0; i < c; i++)
+    for(std::size_t i = 0; i < count; i++)
     {
         sum += *(out_data + i);
     }
+
     delete[] out_data;
-    //delete[] temp;
+    delete[] temp;
     out_data = nullptr;
+    temp = nullptr;
+
     return sum;
 }
