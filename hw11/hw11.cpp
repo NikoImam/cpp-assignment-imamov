ffd8d198c9593340848ecc213b5e8ad4d3669f8c
Niyaz Imamov <niyaz120904@gmail.com>
2023-12-05 22:35:24 +0300

    Some changes

--- hw11/hw11.cpp
+++ hw11/hw11.cpp
@@ -3,9 +3,15 @@
 
 #include <string>
 #include <iostream>
+#include <map>
 
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
+
+std::vector<std::string> images;
+std::vector<std::string> music;
+std::vector<std::string> docs;
+std::vector<std::string> other;
 
 // Код домашнего задания
   // Сортировщик файлов
@@ -57,8 +63,46 @@ namespace fs = boost::filesystem;
     // - Содержимое файлов не важно
     // - Вложенные директории с файлами
     
+void sortFolder(const fs::path& folder)
+{
+    if(fs::exists(folder) && fs::is_directory(folder))
+    {
+        
+        for (const auto& entry : fs::directory_iterator(folder))
+        {
+            fs::file_status st = entry.status();
+            switch (st.type())
+            {
+                case fs::regular_file:
+                    break;
+                case fs::directory_file:
+                    sortFolder(entry);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
 
 int main(int argc, char** argv)
 {
+    
+
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "print usage message")
+        ("path,p", po::value<std::string>()->default_value("path"), "Path to input_folder")
+        ("rules,r", po::value<std::string>()->default_value("rules.txt"), "Path to rules file");
+    
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
+    po::notify(vm);
+
+    fs::path root_folder = "../hw11";
+
+    fs::path folder = root_folder.string() + vm["path"].as<std::string>();
+    std::string rules = vm["rules"].as<std::string>();   
+
     return 0;
 }
